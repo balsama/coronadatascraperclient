ab2275afe3ad296899f3fe441b0ea0dc65a776c1
Adam Balsam <balsama@users.noreply.github.com>
2020-10-25 00:21:12 -0400

    Fix type juggling (#6)
    * Update to v1 version of API
    
    * Increase memory limit
    
    * Fixing type juggling
    
    * PHPCS

--- src/RegionsBase.php
+++ src/RegionsBase.php
@@ -137,7 +137,7 @@ class RegionsBase
                 $cases = $numbers->$type;
             }
             $timestamp = strtotime($date);
-            $dates[$timestamp] = $cases;
+            $dates[$timestamp] = (int) $cases;
         }
         return $dates;
     }
